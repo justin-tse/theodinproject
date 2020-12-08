4e03a9f04b7501697ee454821715bf2251669b3c
justin-tse <xiefahit@gmail.com>
2020-12-08 23:08:08 +0800

    Inheritance vs modules

--- 01-ruby-programming/object-oriented-programming-basics/launch-school-oop-book/inheritance.rb
+++ 01-ruby-programming/object-oriented-programming-basics/launch-school-oop-book/inheritance.rb
@@ -1,71 +1,71 @@
-class Animal
-  def speak
-    "Hello!"
-  end
-end
+# class Animal
+#   def speak
+#     "Hello!"
+#   end
+# end
 
-class GoodDog < Animal
-  attr_accessor :name
+# class GoodDog < Animal
+#   attr_accessor :name
    
-  def initialize(n)
-    self.name = n
-  end
+#   def initialize(n)
+#     self.name = n
+#   end
   
-  def speak
-    super + " from GoodDog class"
-  end
-end
+#   def speak
+#     super + " from GoodDog class"
+#   end
+# end
 
-class Cat < Animal
-end
+# class Cat < Animal
+# end
 
-sparky = GoodDog.new("Sparky")
-paws = Cat.new
-puts sparky.speak
-puts paws.speak
+# sparky = GoodDog.new("Sparky")
+# paws = Cat.new
+# puts sparky.speak
+# puts paws.speak
 
 
-class Animal
-  attr_accessor :name
+# class Animal
+#   attr_accessor :name
 
-  def initialize(name)
-    @name = name
-  end
-end
+#   def initialize(name)
+#     @name = name
+#   end
+# end
 
-class GoodDog < Animal
-  def initialize(color)
-    super
-    @color = color
-  end
-end
+# class GoodDog < Animal
+#   def initialize(color)
+#     super
+#     @color = color
+#   end
+# end
 
-bruno = GoodDog.new("brown")
-p bruno
+# bruno = GoodDog.new("brown")
+# p bruno
 
-class BadDog < Animal
-  def initialize(age, name)
-    super(name)
-    @age = age
-  end
-end
+# class BadDog < Animal
+#   def initialize(age, name)
+#     super(name)
+#     @age = age
+#   end
+# end
 
-p BadDog.new(2, "bear")
+# p BadDog.new(2, "bear")
 
-class Animal
-  def initialize
-  end
-end
+# class Animal
+#   def initialize
+#   end
+# end
 
-class Bear < Animal
-  def initialize(color)
-    super()
-    @color = color
-  end
-end
+# class Bear < Animal
+#   def initialize(color)
+#     super()
+#     @color = color
+#   end
+# end
  
-bear = Bear.new("black")
-p bear
+# bear = Bear.new("black")
+# p bear
 
 module Swimmable
   def swim
@@ -95,4 +95,38 @@ paws = Cat.new
 
 p sparky.swim
 p neemo.swim
-paws.swim
+# paws.swim
+
+module Walkable
+  def walk
+    "I'm walking."
+  end
+end
+
+module Swimmable
+  def swim
+    "I'm swimming"
+  end
+end
+
+module Climable
+  def climb
+    "I'm climbing."
+  end
+end
+
+class Animal
+  include Walkable
+
+  def speak
+    "I'm an animal, and I speak!"
+  end
+end
+
+puts "---Animal method lookup---"
+puts Animal.ancestors
+
+fido = Animal.new
+fido.speak
+
+fido.walk
