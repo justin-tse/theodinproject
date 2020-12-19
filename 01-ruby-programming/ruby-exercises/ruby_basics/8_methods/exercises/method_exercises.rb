cbe20cff679c7e6af01dba50b42464453d6cc1cb
justin-tse <xiefahit@gmail.com>
2020-12-19 15:26:52 +0800

    Finish method exercises

--- 01-ruby-programming/ruby-exercises/ruby_basics/8_methods/exercises/method_exercises.rb
+++ 01-ruby-programming/ruby-exercises/ruby_basics/8_methods/exercises/method_exercises.rb
@@ -6,31 +6,42 @@
 # parameter: number (an integer)
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
-
+def ascii_translator(num)
+  num.chr
+end
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection 
-
+def common_sports(current_sports, favorite_sports)
+  current_sports .intersection(favorite_sports)
+end
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
-
+def alphabetical_list(games)
+  games.uniq.sort
+end
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
-
+def lucky_number(num=7)
+  "Today's lucky number is #{num}"
+end
 
 # method name: #ascii_code
 # parameter: character (a string)
 # return value: the character's ordinal number
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
-
+def ascii_code(character)
+  return 'Input Error' if character.length != 1
+  character.ord
+end
 
 # method name: #pet_pun
 # parameter: animal (a string)
@@ -39,10 +50,17 @@
 # console output: if animal is 'dog', 'Dogs are paw-some!' (awesome)
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
-
+def pet_pun(animal)
+  return puts "Cats are purr-fect!" if animal == 'cat'
+  return puts "Dogs are paw-some!" if animal == 'dog'
+  puts "I think #{animal}s have pet-tential!"
+end
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
+def twenty_first_century?(year)
+  year.between?(2001, 2100)
+end
 
